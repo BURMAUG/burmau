bac27a0a123e121f1f0f032f9f7bdac824f21193
Burmau Garba <burmaud1@gmail.com>
2024-02-12 07:51:20 -0500

    all files in projects

--- src/app/component/projects/projects.component.ts
+++ src/app/component/projects/projects.component.ts
@@ -8,8 +8,8 @@ import {Project} from "./Project";
 })
 export class ProjectsComponent {
   projects = [
-    new Project("Shadow Job Web Scraper", 'https://github.com/BURMAUG/Shadowy',['Python', ' Slack', ' Selenium']),
-    new Project("Car Price Predictor ML", 'https://github.com/BURMAUG/CarPricePrediction', ['Python', ' Numpy', ' Seaborn'])
+    new Project("Shadow Job Web Scraper", 'Nothingham','https://github.com/BURMAUG/Shadowy',['Python', ' Slack', ' Selenium']),
+    new Project("Car Price Predictor ML",'', 'https://github.com/BURMAUG/CarPricePrediction', ['Python', ' Numpy', ' Pandas', ' Seaborn'])
   ]
 
   get projectList(): Project[]{
