f9f4e7d51a1059ff21a457b15eb16557b2823c75
Burmau Garba <burmaud1@gmail.com>
2024-02-12 07:50:51 -0500

    project file updated

--- src/app/component/projects/Project.ts
+++ src/app/component/projects/Project.ts
@@ -1,4 +1,4 @@
 export class Project{
-  constructor(public name: string, public link: string, public tech_stack: string[] = []) {
+  constructor(public name: string, public summary: string, public link: string, public tech_stack: string[] = []) {
   }
 }
